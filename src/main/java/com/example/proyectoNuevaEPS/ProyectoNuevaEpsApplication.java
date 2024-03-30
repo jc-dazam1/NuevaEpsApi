3bc03ebdc59e075d587b697e2a780e2ec0a31016
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-03-30 03:03:20 +0100

    first commit

---
+++ src/main/java/com/example/proyectoNuevaEPS/ProyectoNuevaEpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoNuevaEPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoNuevaEpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoNuevaEpsApplication.class, args);
+	}
+
+}
